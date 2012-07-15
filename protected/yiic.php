e06ae0ffa09c3f67990b6918c23332edb70ecfc9
Suralc <thesurwaveing@googlemail.com>
2012-07-15 12:36:18 +0200

    Fixed small typo

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/yiiCore/framework/yii.php';
+$yiic=dirname(__FILE__).'/yiiCore/framework/yii.php';
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
