60a0f8566f5a6a50b05df72a5c31cb598befac5b
Krish Chandra <krishchan@gmail.com>
2012-08-28 12:15:14 +0530

    Added comments to the migration files

--- protected/migrations/m120826_060739_Migrate_Database.php
+++ protected/migrations/m120826_060739_Migrate_Database.php
@@ -1,30 +1,36 @@
 <?php
 //
+//
 // There are 3 versions of the DL app corresponding to the 3 branches of the app in Git
-// Version 1(master branch): Allows a book to have only one author and category each. No caching is supported 
+// Version 1(master branch):
+//	- Uses PHP script file for authorization data and access control filters for RBAC
+//	- Allows a book to have only one author and category each.
+//	- No support for caching
 // Version 2('Caching-and-Many-Many-relations' branch):
+//	- Uses PHP script file for authorization data and access control filters for RBAC
 //	- Allows a book to have a maximum of 3 authors and categories each
 //	- Caching is enabled
-// Version 3
+// Version 3('Use-Yii-RBAC'):
 //	- Uses Yii's RBAC for access control
-//  - The app's Role component is not used to manage roles
+//	- Allows the books catalog to be searched based on title or author
+//  - Doesn't use the role table. It's there only to make the database backwards compatible
 //
 // Each version has a different database schema
 // This version of (Version 2) of the app has 2 migration files
-//	- This one needs to be used when you are coming up from Version 1 (i.e., master branch)
-// 		- Type yiic migrate up 1 at the command prompt to make the DB schema to be in sync with this version, provided:
-//			1. You have checked out Version 2 ('Caching-and-Many-Many-relations') version of the app
+//	- This one needs to be used when you are migrating up from Version 1 (master branch)
+// 		- To use this migration: Type yiic migrate up 1 at the command prompt to make the DB schema in sync with this version, provided:
+//			1. You have checked out Version 2 ('Caching-and-Many-Many-relations') of the app
 //			2. Your DB schema belongs to Vesion 1 (master)
-//
 // 
 // 	Since this version supports Many_Many relations, the author_id and category_id columns in the book table are no longer useful. Now there are 2
-//	junction tables - one for book/author and the other for book/category. 
+//	junction tables - one for book/author and the other for book/category
 //
 //	The function does the following:
-//	1. Reads the values of these 2 columns from the book table 
-//	2. Drops those 2 columns from the book table
-//	3. Creates 2 junction tables
-//	4. Inserts values obtained in step 1 into the appropriate tables
+//		1. Reads the values of these 2 columns from the book table 
+//		2. Drops those 2 columns from the book table
+//		3. Creates 2 junction tables
+//		4. Inserts values obtained in step 1 into the appropriate tables
+//
 //
 class m120826_060739_Migrate_Database extends CDbMigration
 {
--- protected/migrations/m120826_092147_Migrate_Database.php
+++ protected/migrations/m120826_092147_Migrate_Database.php
@@ -1,26 +1,33 @@
 <?php
 //
+//
 // There are 3 versions of the DL app corresponding to the 3 branches of the app in Git
-// Version 1(master branch): Allows a book to have only one author and category each. No caching is supported 
+// Version 1(master branch):
+//	- Uses PHP script file for authorization data and access control filters for RBAC
+//	- Allows a book to have only one author and category each.
+//	- No support for caching
 // Version 2('Caching-and-Many-Many-relations' branch):
+//	- Uses PHP script file for authorization data and access control filters for RBAC
 //	- Allows a book to have a maximum of 3 authors and categories each
 //	- Caching is enabled
-// Version 3
+// Version 3('Use-Yii-RBAC'):
 //	- Uses Yii's RBAC for access control
-//  - The app's Role component is not used to manage roles
+//	- Allows the books catalog to be searched based on title or author
+//  - Doesn't use the role table. It's there only to make the database backwards compatible
+//
 //
 // Each version has a different database schema
 // This version of (Version 2) of the app has 2 migration files
-//	- This one needs to be used when you are coming down from Version 3 (i.e., 'use-Yii-RBAC' branch)
-// 		- Type yiic migrate down at the command prompt to make the DB schema to be in sync with this version, provided:
-//			1. You have checked out Version 2 ('Caching-and-Many-Many-relations') version of the app
+//	- This one needs to be used when you are coming down from Version 3 ('use-Yii-RBAC' branch)
+// 		- To use this migration, type: yiic migrate down at the command prompt to make the DB schema in sync with this version, provided:
+//			1. You have checked out Version 2 ('Caching-and-Many-Many-relations') of the app
 //			2. Your DB schema belongs to Vesion 3 
 //
 // 
-// 	Here the job is easier
-//	Just drop the 3 tables required for Yii's RBAC
+// 	The function does the following:
+//		1. Drops the 3 tables required for Yii's RBAC
 //
-
+//
 class m120826_092147_Migrate_Database extends CDbMigration
 {
 	public function safeDown()
